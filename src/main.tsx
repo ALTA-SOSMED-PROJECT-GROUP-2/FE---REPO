3031ed413defefe46c1d2627461cde8eb9da3da5
dhanty8 <adeliadhanti1208@gmail.com>
2023-12-05 13:09:50 +0700

    feat: slicing register

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import "./styles/index.css"
 
 import App from './pages/home.tsx'
-import Login from './pages/auth/login.tsx'
+import Login from "./pages/auth/login.tsx"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Register from './pages/auth/register.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Login />
+    <Register />
   </React.StrictMode>,
 )
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -4,18 +4,26 @@ import logo from '@/assets/Chirpy.svg';
 
 const Login = () => {
   return (
-    <div className='w-full h-screen bg-sage4 flex flex-col justify-center items-center'>
-      <form className='flex flex-col w-1/3 bg-white rounded-md p-8'>
+    <div className='h-screen bg-sage4 flex flex-col justify-center items-center'>
+      <form className='flex flex-col w-full md:w-1/3 bg-white rounded-md p-8'>
         <img
-          className=' -mt-28'
+          className='-mt-28'
           src={logo}
           alt='Chirpy Logo'
         />
-        <input placeholder='Phone Number, Full Name or Email' type='email' className='p-3 bg-gray-100 rounded-md mt-8' />
-        <input placeholder='Password' type='password' className='p-3 bg-gray-100 rounded-md mt-8' />
-        <p className='mt-3 text-sage1'>Forgot Password?</p>
-        <Button className='mx-8 mt-8 bg-sage3 hover:bg-sage2'>Login</Button>
-        <p className='mt-3 text-center'>Don't have an account? <span className='text-sage1'>Sign Up</span></p>
+        <input
+          placeholder='Phone Number, Full Name or Email'
+          type='email'
+          className='p-3 bg-gray-100 rounded-md mt-4 sm:mt-8'
+        />
+        <input
+          placeholder='Password'
+          type='password'
+          className='p-3 bg-gray-100 rounded-md mt-4 sm:mt-8'
+        />
+        <p className='mt-2 sm:mt-3 text-sage1 text-sm'>Forgot Password?</p>
+        <Button className='mx-4 sm:mx-8 mt-6 sm:mt-8 bg-sage3 hover:bg-sage2'>Login</Button>
+        <p className='mt-2 sm:mt-3 text-center text-sm'>Don't have an account? <span className='text-sage1'>Sign Up</span></p>
       </form>
     </div>
   );
