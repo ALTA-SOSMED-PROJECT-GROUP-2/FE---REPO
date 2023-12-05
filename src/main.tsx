017635d886a56718f9e88974fcfb0a94d211881e
dhanty8 <adeliadhanti1208@gmail.com>
2023-12-05 13:20:34 +0700

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import "./styles/index.css"
 
-import App from './pages/home.tsx'
-import Login from "./pages/auth/login.tsx"
+import App from '@/routes'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Register from './pages/auth/register.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Register />
+    <App />
   </React.StrictMode>,
 )
